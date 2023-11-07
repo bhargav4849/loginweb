3b29a1057bb6b2897dd97351470c82004516ac94
bhargav4849 <144525551+bhargav4849@users.noreply.github.com>
2023-11-07 14:14:58 +0530

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        #Azure Kubernetes Service
+        AWS Devops
       </div>
       <div class="col-lg-6">
       </div>
